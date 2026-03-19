9261c2a8254dc88675647d01da2d4c7e715c6671
Nikola Stankovic <jonnywintech@gmail.com>
2026-03-19 22:37:51 +0100

    back in progress

---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+void _takePicture() {}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          width: 1,
+          color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2),
+        ),
+      ),
+      height: 250,
+      width: double.infinity,
+      alignment: Alignment.center,
+      child: TextButton.icon(
+        onPressed: _takePicture,
+        icon: Icon(Icons.camera),
+        label: Text('Take picture'),
+      ),
+    );
+  }
+}
