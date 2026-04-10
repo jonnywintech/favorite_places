cbd1ef79e8f9cbd37a7115dfa6698e38ccebcf8d
Nikola Stankovic <jonnywintech@gmail.com>
2026-04-10 21:27:51 +0200

    adding more functionality

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
-  const ImageInput({super.key});
+  const ImageInput({super.key, required this.onPickImage});
+
+  final Function(File) onPickImage;
 
   @override
   _ImageInputState createState() => _ImageInputState();
@@ -27,6 +29,7 @@ class _ImageInputState extends State<ImageInput> {
     setState(() {
       _selectedImage = File(pickedImage.path);
     });
+    widget.onPickImage(_selectedImage!);
   }
 
   @override
